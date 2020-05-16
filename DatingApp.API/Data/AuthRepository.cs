72dfad3c82ca60172e1676743913365b091eacf3
Mashud Ahmed <mashudahmed1980@gmail.com>
2020-05-15 22:48:25 -0400

    Edited UserExists Method on AuthRepository.cs Class

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -64,9 +64,12 @@ namespace DatingApp.API.Data
             }
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new System.NotImplementedException();
+            if(await _context.Users.AnyAsync(x => x.Username == username))
+            return true;
+
+            return false;
         }
     }
 }

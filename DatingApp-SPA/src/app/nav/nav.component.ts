3e4ce038e09946a5b3ee72f593edc085ba243b41
Mashud Ahmed <mashudahmed1980@gmail.com>
2020-05-28 14:47:29 -0400

    Validated nav.component.html & Created _sevice folder

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  model: any = {}; // It store username and password which lend be ableto pass to an ever method.
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  login(){
+    console.log(this.model);
   }
 
 }

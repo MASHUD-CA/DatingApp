f781cb6f4e90ad70b0781c9ccd70b32cd7383e8a
Mashud Ahmed <mashudahmed1980@gmail.com>
2020-06-01 12:54:42 -0400

    Added authService as Costructor & login() at nav.component.ts

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-nav',
@@ -8,12 +10,16 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   model: any = {}; // It store username and password which lend be ableto pass to an ever method.
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
   login(){
-    console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in successfully');
+    }, eror => {
+      console.log('Failed to login');
+    });
   }
 
 }
